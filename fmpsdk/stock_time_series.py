9bf4acb7fbc31bf1967a5cb14e3aea8c676901f1
Dax Mickelson <dmickels@cisco.com>
2021-02-20 14:16:06 -0700

    Add survivorship bias free EOD method.

--- fmpsdk/stock_time_series.py
+++ fmpsdk/stock_time_series.py
@@ -2,6 +2,7 @@ from .general import __quotes
 
 from .url_methods import (
     __return_json_v3,
+    __return_json_v4,
     __validate_exchange,
 )
 
@@ -58,3 +59,17 @@ def historical_stock_split(apikey: str, symbol: str) -> typing.List[typing.Dict]
     path = f"historical-price-full/stock_split/{symbol}"
     query_vars = {"apikey": apikey}
     return __return_json_v3(path=path, query_vars=query_vars)
+
+
+def historical_survivorship_bias_free_eod(apikey: str, symbol: str, date: str) -> typing.List[typing.Dict]:
+    """
+    Query FMP /historical-price-full/<ticker>/<date> API
+
+    :param apikey: Your API key.
+    :param symbol: Company ticker.
+    :param date: str YYYY-MM-DD
+    :return: A list of dictionaries.
+    """
+    path = f"historical-price-full/{symbol}/{date}"
+    query_vars = {"apikey": apikey}
+    return __return_json_v4(path=path, query_vars=query_vars)
