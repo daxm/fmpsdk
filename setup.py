4ea8c4ad0f2fcf6c260cd9391908fbb55aafad95
Dax Mickelson <dmickelson@zscaler.com>
2023-05-22 08:12:26 -0600

    update serial ran black and isort

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import find_packages, setup
 
+__version__ = "20230522.0"
 __author__ = "Dax Mickelson"
-__author_email = "fmp@daxm.net"
+__author_email__ = "fmp@daxm.net"
 __license__ = "BSD"
+__package_name__ = "fmpsdk"
+__description__ = "SDK for interacting with FMP's APIs."
+__long_description__ = """Interact with Financial Modeling Prep's APIs."""
+__url__ = "https://github.com/daxm/fmpsdk"
 
 setup(
-    name="fmpsdk",
-    version="20230312.0",
-    description="SDK for interacting with FMP's APIs.",
-    long_description="""Interact with Financial Modeling Prep's APIs.""",
-    url="https://github.com/daxm/fmpsdk",
-    author="Dax Mickelson",
-    author_email="fmp@daxm.net",
-    license="BSD",
+    name=__package_name__,
+    version=__version__,
+    description=__description__,
+    long_description=__long_description__,
+    url=__url__,
+    author=__author__,
+    author_email=__author_email__,
+    license=__license__,
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Plugins",
@@ -32,5 +37,5 @@ setup(
     install_requires=["python-dotenv", "requests"],
     python_requires=">=3.6",
     package_data={},
-    data_files=None,
+    data_files=[],
 )
